ce9fd0d626f94433e43a2dbb1fa74d81991f959f
Versim <wojciechkrzos67@gmail.com>
2023-11-05 17:26:42 +0100

    doc: add time complexity estimation of initSegment function

--- Semester 3/Programming Paradigms/Tutorials/tut2/ex5.ml
+++ Semester 3/Programming Paradigms/Tutorials/tut2/ex5.ml
@@ -6,6 +6,8 @@ let rec initSegment (list1, list2) =
   | _ -> false
 ;;
 
+(* Test cases *)
+
 initSegment ([], [1; 2; 3]);;  (* Expect: true, an empty list is an initial segment of any list. *)
 initSegment ([1; 2; 3], []);;  (* Expect: false, an empty list cannot be an initial segment of a non-empty list. *)
 initSegment ([1], [1; 2; 3]);;  (* Expect: true, [1] is an initial segment of [1; 2; 3]. *)
@@ -18,4 +20,28 @@ initSegment (["x"; "y"; "z"], ["x"; "y"; "z"]);;  (* Expect: true, ["x"; "y"; "z
 initSegment ([1; 2; 3; 4], [1; 2; 3; 4]);;  (* Expect: true, [1; 2; 3; 4] is an initial segment of itself. *)
 initSegment ([1; 2; 3; 4], [1; 2; 3]);;  (* Expect: false, [1; 2; 3; 4] is not an initial segment
 
+(* 
 
+Expected O() complexity 
+
+The time complexity of the `initSegment` function can be analyzed as follows:
+
+1. The function is a recursive function that takes two lists, `list1` and `list2`, as input.
+
+2. In each recursive call, the function pattern matches on the two input lists, which involves checking the length and comparing the first elements.
+
+3. There are four cases in the pattern matching:
+   a. If `list1` is empty, the function returns `true`.
+   b. If `list2` is empty, the function returns `false`.
+   c. If the first elements of `list1` and `list2` are equal, the function makes a recursive call with the tails of both lists (i.e., `initSegment (xs, ys)`).
+   d. In all other cases, the function returns `false`.
+
+4. In the worst case, the function will have to traverse both lists completely to determine if one is an initial segment of the other.
+
+The time complexity of this function can be analyzed as O(min(n, m)), where "n" is the length of `list1` and "m" is the length of `list2`. This is because the function will need to check a minimum of the length of the shorter list. In the worst case, if one list is a prefix of the other, it will have to traverse both lists, but it will not have to examine elements beyond the length of the shorter list.
+
+In big O notation, O(min(n, m)) simplifies to O(min(n, m)).
+
+So, the time complexity of the `initSegment` function is O(min(n, m)), where "n" is the length of `list1` and "m" is the length of `list2`.
+
+*)

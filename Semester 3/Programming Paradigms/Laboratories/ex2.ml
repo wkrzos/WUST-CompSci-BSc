bbdb35d588052b12018e8781e58a3194b8372d44
Wojciech Krzos <wojciechkrzos67@gmail.com>
2023-11-07 19:43:00 +0100

    Update Laboratories

---
+++ Semester 3/Programming Paradigms/Laboratories/ex2.ml
@@ -0,0 +1,10 @@
+let split3Rec list =
+1. calculate list length
+2. get /3 with no remainder + the number must be divisible by 3
+3. save the number of elements in one smaller  list
+3. recursive method, match/if, 
+;;
+
+let split3Tail list =
+1
+;;

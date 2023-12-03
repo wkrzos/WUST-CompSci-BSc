f19597fb44588037acff06c16e2041ddef0833ff
Versim <wojciechkrzos67@gmail.com>
2023-12-03 23:08:25 +0100

    ref: move SinNode and CosNode to OperatorNode

--- Semester 3/Efficient Programming Techniques/Laboratories/lab3/src/Node/OperatorNode.cpp
+++ Semester 3/Efficient Programming Techniques/Laboratories/lab3/src/Node/OperatorNode.cpp
@@ -123,3 +123,83 @@ Node* OperatorNode::getLeft() {
 Node* OperatorNode::getRight() {
     return right;
 }
+
+#include "SinNode.h"
+
+SinNode::SinNode(Node* op) : operand(op) {}
+
+SinNode::~SinNode() {
+    delete operand;
+}
+
+double SinNode::evaluate() const {
+    return std::sin(operand->evaluate());
+}
+
+void SinNode::print() const {
+    std::cout << "sin(";
+    operand->print();
+    std::cout << ")";
+}
+
+void SinNode::printTree(int indent) const {
+    std::cout << std::string(indent, ' ') << "SinNode" << std::endl;
+    operand->printTree(indent + 2);
+}
+
+void SinNode::printPrefix() const {
+    std::cout << "sin ";
+    operand->printPrefix();
+}
+
+void SinNode::printVariables(std::set<std::string>& variableSet) const {
+    operand->printVariables(variableSet);
+}
+
+size_t SinNode::countVariables() const {
+    return operand->countVariables();
+}
+
+double SinNode::evaluateWithValues(const std::map<std::string, double>& values) const {
+    return std::sin(operand->evaluateWithValues(values));
+}
+
+#include "CosNode.h"
+
+CosNode::CosNode(Node* op) : operand(op) {}
+
+CosNode::~CosNode() {
+    delete operand;
+}
+
+double CosNode::evaluate() const {
+    return std::cos(operand->evaluate());
+}
+
+void CosNode::print() const {
+    std::cout << "cos(";
+    operand->print();
+    std::cout << ")";
+}
+
+void CosNode::printTree(int indent) const {
+    std::cout << std::string(indent, ' ') << "CosNode" << std::endl;
+    operand->printTree(indent + 2);
+}
+
+void CosNode::printPrefix() const {
+    std::cout << "cos ";
+    operand->printPrefix();
+}
+
+void CosNode::printVariables(std::set<std::string>& variableSet) const {
+    operand->printVariables(variableSet);
+}
+
+size_t CosNode::countVariables() const {
+    return operand->countVariables();
+}
+
+double CosNode::evaluateWithValues(const std::map<std::string, double>& values) const {
+    return std::cos(operand->evaluateWithValues(values));
+}

53bbf4c770e9dab2e5fc6c825434dcd91f3822b9
Versim <wojciechkrzos67@gmail.com>
2023-12-13 02:21:00 +0100

    ref: remove redundant comments

--- Semester 3/Programming Paradigms/Laboratories/list3/ex1.ml
+++ Semester 3/Programming Paradigms/Laboratories/list3/ex1.ml
@@ -1,16 +1,3 @@
-(* Foundation of the code 
-
-let rec map2 f xs ys =
-  match (xs, ys) with
-    | [], [] -> []
-    |  _, [] -> []
-    | [],  _ -> []
-    | x::xs, y::ys -> (f x y) :: (map2 f xs ys)
-;; 
-
-*)
-
-(*Solution 1*)
 let (><) list1 list2 f =
   let rec map2 xs ys =
     match (xs, ys) with

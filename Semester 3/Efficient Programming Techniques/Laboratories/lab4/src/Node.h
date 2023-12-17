41de8ac0334feedca9f596ca509614caacc053df
Versim <wojciechkrzos67@gmail.com>
2023-12-18 00:27:01 +0100

    ref: node 2

--- Semester 3/Efficient Programming Techniques/Laboratories/lab4/src/Node.h
+++ Semester 3/Efficient Programming Techniques/Laboratories/lab4/src/Node.h
@@ -1,43 +1,46 @@
 #ifndef NODE
 #define NODE
+
 #include <string>
-#include <vector>
 
 enum TYPE
-{	
-	CONSTANT,
-	VARIABLE,
-	OPERATION,
-	NO_TYPE
+{
+    CONSTANT,
+    VARIABLE,
+    OPERATION,
+    NO_TYPE
 };
 
 class Node
 {
 private:
-	int numberOfNodes;
-	TYPE type;
-	std::string key;
-	Node* nodes;
+    int numberOfNodes;
+    TYPE type;
+    std::string key;
+    Node* nodes;
 
 public:
-	Node();
-	Node(const Node& otherNode);
-	~Node();
+    // Constructors and Destructor
+    Node();
+    Node(const Node& otherNode);
+    ~Node();
 
-	Node& operator=(const Node& newValue);
+    // Assignment Operator
+    Node& operator=(const Node& newValue);
 
-	std::string toString() const;
+    // Accessor Methods
+    std::string toString() const;
+    Node* getNodes() const;
+    Node* getNode(int index) const;
+    std::string getValue() const;
+    int getNumberOfNodes() const;
+    TYPE getNodeType() const;
 
-	Node* getNodes() const;
-	Node* getNode(int index) const;
-	std::string getValue() const;
-	int getNumberOfNodes() const;
-	TYPE getNodeType() const;
-
-	void setNode(int index, Node& node);
-	void setValue(std::string value);
-	void setNumberOfNodes(int numberOfNodes);
-	void setNodeType(TYPE type);
+    // Mutator Methods
+    void setNode(int index, Node& node);
+    void setValue(std::string value);
+    void setNumberOfNodes(int numberOfNodes);
+    void setNodeType(TYPE type);
 };
 
 #endif

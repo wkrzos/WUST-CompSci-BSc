6d851ae4af09dd67975e6fd27000f16649a58ef0
Versim <wojciechkrzos67@gmail.com>
2024-01-03 21:24:54 +0100

    feat: add assignment operator for MySmartPointer

--- Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/MySmartPointer.h
+++ Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/MySmartPointer.h
@@ -28,10 +28,30 @@ public:
     T& operator*() { return (*pc_pointer); }
     T* operator->() { return (pc_pointer); }
 
+    MySmartPointer& operator=(const MySmartPointer& other)
+    {
+        if (this != &other) // Check for self-assignment
+        {
+            // Decrement the current reference count
+            if (pc_counter->dec() == 0)
+            {
+                delete pc_pointer;
+                delete pc_counter;
+            }
+
+            // Copy the pointer and counter from the other object
+            pc_pointer = other.pc_pointer;
+            pc_counter = other.pc_counter;
+            pc_counter->add();
+        }
+        return *this;
+    }
+
 private:
     RefCounter* pc_counter;
     T* pc_pointer;
 };
+
 
 class RefCounter
 {

ab106de8ada4634be299e1806f3d6a797c142f36
Versim <wojciechkrzos67@gmail.com>
2024-01-08 14:53:12 +0100

    test: add tests for MySmartPointer class

--- Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/Main.cpp
+++ Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/Main.cpp
@@ -44,20 +44,20 @@ public:
 int main() {
     // Creating a new Test object with a smart pointer
     MySmartPointer<Test> ptr1(new Test());
-    std::cout << "Counter after ptr1 creation: " << ptr1.getCounter() << std::endl;
+    std::cout << "Counter ptr1 after ptr1 creation: " << ptr1.getCounter() << std::endl;
 
     // Using the -> operator to call a method
     ptr1->greet();
 
     // Creating another smart pointer pointing to the same object
     MySmartPointer<Test> ptr2 = ptr1;
-    std::cout << "Counter after ptr2 creation: " << ptr1.getCounter() << std::endl;
-    std::cout << "Counter after ptr2 creation: " << ptr2.getCounter() << std::endl;
+    std::cout << "Counter ptr1 after ptr2 creation: " << ptr1.getCounter() << std::endl;
+    std::cout << "Counter ptr2 after ptr2 creation: " << ptr2.getCounter() << std::endl;
 
     // Creating a third smart pointer using the duplicate method
     MySmartPointer<Test> ptr3 = ptr1.duplicate();
-    std::cout << "Counter after ptr3 creation: " << ptr1.getCounter() << std::endl;
-    std::cout << "Counter after ptr3 creation: " << ptr3.getCounter() << std::endl;
+    std::cout << "Counter ptr1 after ptr3 creation: " << ptr1.getCounter() << std::endl;
+    std::cout << "Counter ptr3 after ptr3 creation: " << ptr3.getCounter() << std::endl;
 
     // The Test object will be destroyed when the last smart pointer goes out of scope
     return 0;

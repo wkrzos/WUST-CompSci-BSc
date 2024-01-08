1679d75fda3080c46373dbfcca39839583f5771d
Versim <wojciechkrzos67@gmail.com>
2024-01-08 14:52:54 +0100

    ref: improve naming

--- Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/modification/MySmartPointer.h
+++ Semester 3/Efficient Programming Techniques/Laboratories/lab5/src/modification/MySmartPointer.h
@@ -1,71 +1,74 @@
 #ifndef MYSMARTPOINTER_H
 #define MYSMARTPOINTER_H
+
 #include "RefCounter.h"
 
 template <typename T>
 class MySmartPointer
 {
 public:
-  MySmartPointer(T *pcPointer);
-  MySmartPointer();
-  MySmartPointer(const MySmartPointer &pcOther);
-  ~MySmartPointer();
+    MySmartPointer(T* pcPointer);
+    MySmartPointer();
+    MySmartPointer(const MySmartPointer& pcOther);
+    ~MySmartPointer();
 
-  MySmartPointer &operator=(const MySmartPointer &pcOther);
-  T &operator*();
-  T *operator->();
+    MySmartPointer& operator=(const MySmartPointer& pcOther);
 
-  MySmartPointer<T> duplicate(); //Modification
-  int getCounter() const;
+    T& operator*();
+    T* operator->();
+
+    MySmartPointer<T> duplicate() const; // Modification
+    int getCounter() const;
 
 private:
-  RefCounter *pc_counter;
-  T *pc_pointer;
+    RefCounter* pc_counter;
+    T* pc_pointer;
 };
 
 template <typename T>
-MySmartPointer<T>::MySmartPointer(T *pcPointer) : pc_counter(new RefCounter()), pc_pointer(pcPointer)
+MySmartPointer<T>::MySmartPointer(T* pcPointer) : pc_counter(new RefCounter()), pc_pointer(pcPointer)
 {
-  pc_counter->add();
+    pc_counter->add();
 }
 
 template <typename T>
-inline MySmartPointer<T>::MySmartPointer()
+MySmartPointer<T>::MySmartPointer() : pc_counter(new RefCounter()), pc_pointer(nullptr)
 {
-  pc_counter = new RefCounter();
-  pc_pointer = nullptr;
 }
 
 template <typename T>
-MySmartPointer<T>::MySmartPointer(const MySmartPointer &pcOther) : pc_counter(pcOther.pc_counter), pc_pointer(pcOther.pc_pointer)
+MySmartPointer<T>::MySmartPointer(const MySmartPointer& pcOther) : pc_counter(pcOther.pc_counter), pc_pointer(pcOther.pc_pointer)
 {
-  pc_counter->add();
+    pc_counter->add();
 }
 
 template <typename T>
 MySmartPointer<T>::~MySmartPointer()
 {
-  if (pc_counter->dec() == 0)
-  {
-    delete pc_pointer;
-    delete pc_counter;
-  }
+    if (pc_counter->dec() == 0)
+    {
+        delete pc_pointer;
+        delete pc_counter;
+    }
 }
 
 template <typename T>
-MySmartPointer<T> &MySmartPointer<T>::operator=(const MySmartPointer &pcOther)
+MySmartPointer<T>& MySmartPointer<T>::operator=(const MySmartPointer& pcOther)
 {
-  if (pc_counter->dec() == 0)
-  {
-    delete pc_pointer;
-    delete pc_counter;
-  }
+    if (this == &pcOther) // Check for self-assignment
+        return *this;
 
-  pc_counter = pcOther.pc_counter;
-  pc_pointer = pcOther.pc_pointer;
-  pc_counter->add();
+    if (pc_counter->dec() == 0)
+    {
+        delete pc_pointer;
+        delete pc_counter;
+    }
 
-  return *this;
+    pc_counter = pcOther.pc_counter;
+    pc_pointer = pcOther.pc_pointer;
+    pc_counter->add();
+
+    return *this;
 }
 
 template <typename T>
@@ -81,15 +84,15 @@ T* MySmartPointer<T>::operator->()
 }
 
 template <typename T>
-inline MySmartPointer<T> MySmartPointer<T>::duplicate()
+MySmartPointer<T> MySmartPointer<T>::duplicate() const
 {
-  return MySmartPointer<T>(*this);
+    return MySmartPointer(*this);
 }
 
 template <typename T>
 int MySmartPointer<T>::getCounter() const
 {
-  return pc_counter->get();
+    return pc_counter->get();
 }
 
 #endif

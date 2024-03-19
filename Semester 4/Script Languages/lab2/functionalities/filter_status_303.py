742ec8eedd0ec0122e53287b9934b3b6df358f58
Versim <wojciechkrzos67@gmail.com>
2024-03-19 18:33:46 +0100

    fix: fix filtering by wrong numbers

--- Semester 4/Script Languages/lab2/functionalities/filter_status_303.py
+++ Semester 4/Script Languages/lab2/functionalities/filter_status_303.py
@@ -1,15 +1,15 @@
 import sys
 
-def filter_status_300():
+def filter_status_302():
     for line in sys.stdin:
         try:
             status_code = int(line.strip())  # Convert the read line to an integer
-            if status_code == 300:
-                print(status_code)  # Print status code if it is 300
+            if status_code == 302:
+                print(status_code)  # Print status code if it is 302
         except ValueError:
             print(f"Could not convert {line.strip()} to an integer", file=sys.stderr)
             # Handle the case where the line is not an integer,
             # You might want to print an error or pass silently
             pass
 
-filter_status_300()
+filter_status_302()

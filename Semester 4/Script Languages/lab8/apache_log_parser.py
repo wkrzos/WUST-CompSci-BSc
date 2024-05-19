9e46e2f9cafb59aeacccdffc0bd094282a2fb266
Versim <wojciechkrzos67@gmail.com>
2024-05-20 00:42:15 +0200

    ref: move apache log parser from gui.py to apache_log_parser.py

---
+++ Semester 4/Script Languages/lab8/apache_log_parser.py
@@ -0,0 +1,27 @@
+import re
+from datetime import datetime
+
+def parse_apache_log(log_line):
+    log_pattern = r'(\S+) - - \[(.*?)\] "(.*?)" (\d{3}) (\d+|-)'
+    match = re.match(log_pattern, log_line)
+    if match is None:
+        raise ValueError("Invalid log line")
+    groups = match.groups()
+
+    timestamp_format = "%d/%b/%Y:%H:%M:%S %z"
+    timestamp = datetime.strptime(groups[1], timestamp_format)
+    method, resource = groups[2].split(' ')[0], groups[2].split(' ')[1]
+
+    status_code = int(groups[3])
+    response_size = int(groups[4]) if groups[4].isdigit() else 0
+
+    return {
+        'remote_host': groups[0],
+        'date': timestamp.strftime('%Y-%m-%d'),
+        'time': timestamp.strftime('%H:%M:%S'),
+        'timezone': timestamp.strftime('%z'),
+        'method': method,
+        'resource': resource,
+        'status_code': status_code,
+        'response_size': response_size
+    }
--- Semester 4/Script Languages/lab8/gui.py
+++ Semester 4/Script Languages/lab8/gui.py
@@ -1,34 +1,9 @@
 import sys
-import re
 from datetime import datetime
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QListWidget, QLabel, QHBoxLayout, QPushButton, QFileDialog, QFormLayout, QDateEdit, QSplitter, QLineEdit)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QListWidget, QLabel, QHBoxLayout, QPushButton, QFileDialog, QFormLayout, QDateEdit, QSplitter, QLineEdit
 from PyQt5.QtCore import QDate, Qt
 from PyQt5.QtGui import QIcon
-
-def parse_apache_log(log_line):
-    log_pattern = r'(\S+) - - \[(.*?)\] "(.*?)" (\d{3}) (\d+|-)'
-    match = re.match(log_pattern, log_line)
-    if match is None:
-        raise ValueError("Invalid log line")
-    groups = match.groups()
-
-    timestamp_format = "%d/%b/%Y:%H:%M:%S %z"
-    timestamp = datetime.strptime(groups[1], timestamp_format)
-    method, resource = groups[2].split(' ')[0], groups[2].split(' ')[1]
-
-    status_code = int(groups[3])
-    response_size = int(groups[4]) if groups[4].isdigit() else 0
-
-    return {
-        'remote_host': groups[0],
-        'date': timestamp.strftime('%Y-%m-%d'),
-        'time': timestamp.strftime('%H:%M:%S'),
-        'timezone': timestamp.strftime('%z'),
-        'method': method,
-        'resource': resource,
-        'status_code': status_code,
-        'response_size': response_size
-    }
+import apache_log_parser as alp
 
 class LogBrowserApp(QMainWindow):
     def __init__(self):
@@ -131,7 +106,7 @@ class LogBrowserApp(QMainWindow):
     def read_log_file(self, file_path):
         print(f"Reading log file: {file_path}")
         with open(file_path, 'r') as file:
-            self.all_log_entries = [parse_apache_log(line.strip()) for line in file if line.strip()]
+            self.all_log_entries = [alp.parse_apache_log(line.strip()) for line in file if line.strip()]
             self.display_logs(self.all_log_entries)
             self.update_navigation_buttons()
 
